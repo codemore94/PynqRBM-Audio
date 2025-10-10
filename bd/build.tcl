da4e5796f3b986ad1ef1ffb2a0f445e340bd7018
Mikko Saari <mikko.l.saari@tuni.fi>
2025-10-10 21:34:13 +0300

    build_config

---
+++ bd/build.tcl
@@ -0,0 +1,13 @@
+create_project bolt_ear ./bolt_ear -part xc7z020clg400-1
+set_property board_part digilentinc.com:pynq-z1:part0:1.0 [current_project]
+read_verilog -sv [glob ../rtl/*.sv]
+read_xdc ../bd/constraints.xdc
+# Block design (PS + AXI interconnect + UART/Timer + BRAM + accel)
+source ../bd/create_bd.tcl
+update_compile_order -fileset sources_1
+synth_design -top top_pl -part xc7z020clg400-1
+opt_design
+place_design
+route_design
+write_bitstream -force bolt_ear.bit
+write_hw_platform -fixed -include_bit -force bolt_ear.xsa
