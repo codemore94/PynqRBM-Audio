e59c358cf73929538a009f684ed1be95b5c02f6a
Mikko Saari <mikko.l.saari@tuni.fi>
2025-10-11 23:49:20 +0300

    name_convention

--- fpga/scripts/build_project.tcl
+++ fpga/scripts/build_project.tcl
@@ -1,4 +1,4 @@
-create_project bolt_ear ./bolt_ear -part xc7z020clg400-1
+create_project pynq_audio_rbm ./pynq_audio_rbm -part xc7z020clg400-1
 set_property board_part digilentinc.com:pynq-z1:part0:1.0 [current_project]
 read_verilog -sv [glob ../rtl/*.sv]
 read_xdc ../bd/constraints.xdc
@@ -8,5 +8,5 @@ synth_design -top rbm_core_min -part xc7z020clg400-1
 opt_design
 place_design
 route_design
-write_bitstream -force bolt_ear.bit
-write_hw_platform -fixed -include_bit -force bolt_ear.xsa
+write_bitstream -force pynq_audio_rbm.bit
+write_hw_platform -fixed -include_bit -force pynq_audio_rbm.xsa
