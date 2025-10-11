0e4f6d6547a555d060763e395a621308e284c6e1
Mikko Saari <mikko.l.saari@tuni.fi>
2025-10-11 03:15:33 +0300

    small_comments

--- PSEUDO_FREERTOS_driver.c
+++ PSEUDO_FREERTOS_driver.c
@@ -2,10 +2,10 @@
 void vAccelTask(void* arg) {
   for(;;) {
     // Wait frame_ready (from AudioInTask or PS)
-    load_frame_to_axis();               // write to AXIS-S FIFO
+    load_frame_to_axis();               // write to AXIS-Slave FIFO
     rbm_regs->CONTROL = CTRL_START;
     ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // ISR on DONE sets notify
-    read_logits_from_axis();            // AXIS-M
+    read_logits_from_axis();            // AXIS-Master
     send_result_to_ps();                // mailbox or UART
   }
 }
